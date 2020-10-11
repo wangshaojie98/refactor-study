4e0b195dd030126711903022125fc8499e3581e1
wangshaojie <15890534176@163.com>
2020-10-11 11:17:05 +0800

    提炼amountFor函数

---
+++ 01-first-demo/04-split-statement-function/index.js
@@ -0,0 +1,66 @@
+const fs = require("fs");
+const path = require("path")
+const playsJson = fs.readFileSync(path.resolve(__dirname, "./plays.json"), {encoding: "utf8"});
+const invoicesJson = fs.readFileSync(path.resolve(__dirname, "./invoices.json"), {encoding: "utf8"});
+
+const plays = JSON.parse(playsJson);
+const invoices = JSON.parse(invoicesJson);
+
+function amountFor(perf, play) {
+	let thisAmount = 0;
+
+	switch (play.type) {
+		case 'tragedy':
+			thisAmount = 40000;
+			if (perf.audience > 30) {
+				thisAmount += 1000 * (perf.audience - 30);
+			}
+			break;
+		case 'comedy':
+			thisAmount = 30000;
+			if (perf.audience > 20) {
+				thisAmount += 10000 + 500 * (perf.audience - 20);
+			}
+			thisAmount += 300 * perf.audience;
+			break;
+		default:
+			throw new Error(`unknown type: ${play.type}`);
+	}
+
+	return thisAmount;
+}
+
+function statement(invoice, plays) {
+	let totalAmount = 0;
+	let volumeCredits = 0;
+	let result = `Statement for ${invoice.customer}\n`;
+	const format = new Intl.NumberFormat('en-US', {
+		style: 'currency',
+		currency: 'USD',
+		minimumFractionDigits: 2
+  }).format;
+  console.log(invoice);
+	for (let perf of invoice.performances) {
+		const play = plays[perf.playID];
+		// 改善之后的语句
+		const thisAmount = amountFor(perf, play);
+    
+		// add volume credits
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    
+		// add extra credit for every ten comedy attendees
+    if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    
+		// print line for this order
+		result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
+		totalAmount += thisAmount;
+  }
+  
+	result += `Amount owed is ${format(totalAmount / 100)}\n`;
+  result += `You earned ${volumeCredits} credits\n`;
+  
+	return result;
+}
+
+const result = statement(invoices[0], plays);
+console.log(result);
